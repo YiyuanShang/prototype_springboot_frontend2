8dd404e3a284878eb9581788399ddb0e1257b275
YiyuanShang <yiyuan.shang@mail.mcgill.ca>
2021-07-08 18:33:37 -0400

    first commit

---
+++ src/main/java/com/athensoft/prototype/PrototypeSpringbootFrontend2Application.java
@@ -0,0 +1,13 @@
+package com.athensoft.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeSpringbootFrontend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeSpringbootFrontend2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/athensoft/prototype/PrototypeSpringbootFrontend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athensoft.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeSpringbootFrontend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
